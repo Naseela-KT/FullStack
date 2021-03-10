c7109e5b907e15c7c0266ab37aea290e9696f5b6
Naseela-KT <75978405+Naseela-KT@users.noreply.github.com>
2021-03-10 08:22:28 +0530

    changes

--- insertmultidata.php
+++ insertmultidata.php
@@ -8,10 +8,11 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die ("Connection failed:" .$conn->connect_error);
 }
-$sql = "INSERT INTO AiclTable (firstname, lastname, email, contact)
-VALUES ('Shilpa', 'A N', 'mnhj@gmail.com', 9879876540)";
+
 $sql = "INSERT INTO AiclTable (firstname, lastname, email, contact)
 VALUES ('Irfana', 'K V', 'bhgt@gmail.com', 9789786780)";
+$sql .= "INSERT INTO AiclTable (firstname, lastname, email, contact)
+VALUES ('Shilpa', 'A N', 'mnhj@gmail.com', 9879876540)";
 
 
 if ($conn->multi_query($sql) === TRUE) {
