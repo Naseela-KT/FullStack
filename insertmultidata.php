3bafd2d660b2041c7cfc5fb5032c45d6994acbe0
Naseela-KT <75978405+Naseela-KT@users.noreply.github.com>
2021-03-10 08:06:34 +0530

    changes

--- insertmultidata.php
+++ insertmultidata.php
@@ -12,11 +12,11 @@
      die("Connection faied:" . $conn->connect_error);
  }
 
- $sql = "INSERT INTO AiclTable(fisrtname,lastname,email,contact)
+ $sql = "INSERT INTO AiclTable(firstname,lastname,email,contact)
  VALUES('Shilpa', 'A N', 'nmjl@gmail.com', 9678905432)";
- $sql = "INSERT INTO AiclTable(fisrtname,lastname,email,contact)
+ $sql = "INSERT INTO AiclTable(firstname,lastname,email,contact)
  VALUES('Irfana', 'K V', 'efgh@gmail.com', 9898767609)";
- $sql = "INSERT INTO AiclTable(fisrtname,lastname,email,contact)
+ $sql = "INSERT INTO AiclTable(firstname,lastname,email,contact)
  VALUES('Fathima', 'Dilsha', 'ijk@gmail.com', 9876543210)";
  
  if ($conn->multi_query($sql) === TRUE) {
