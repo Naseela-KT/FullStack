031f3a6831affcc0ae863a8f38f01eed0c444061
Naseela-KT <75978405+Naseela-KT@users.noreply.github.com>
2021-03-13 12:36:25 +0530

    update

--- aiclselect.php
+++ aiclselect.php
@@ -11,15 +11,17 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM AICLform_Table";
+$sql = "SELECT * FROM AICLformTable";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
     echo "<table border='1'>";
-    echo"<tr><th>Firstname</th><th>Contact</th><th>Message</th></tr>";
+    echo"<tr><th>Firstname</th><th>Lastname</th><th>email</th><th>Contact</th><th>Message</th></tr>";
     echo "<tr><td>" . $row['firstname'] . "</td>";
+    echo "<tr><td>" . $row['lastname'] . "</td>";
+    echo "<tr><td>" . $row['email'] . "</td>";
     echo "<td>" . $row['contact'] . "</td>";
     echo "<td>" . $row['messagetext'] . "</td></tr>";
     echo "</table>";
