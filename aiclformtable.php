9b4360003197d42dd2ace379ffc058765c784251
Naseela-KT <75978405+Naseela-KT@users.noreply.github.com>
2021-03-13 12:43:19 +0530

    update

--- aiclformtable.php
+++ aiclformtable.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// sql to create table
+// sql to create table 
 $sql = "CREATE TABLE AICLformTable(
 firstname VARCHAR(30) NOT NULL,
 lastname VARCHAR(30) NOT NULL,
